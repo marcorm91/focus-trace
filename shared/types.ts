5cbe2148b4714e38d8fa5e77aeb87610f378c117
Marco Romero <marco_antonio88_9@hotmail.com>
2026-08-25 09:29:22 +0200

    Add structured focus walk report data

--- shared/types.ts
+++ shared/types.ts
@@ -96,6 +96,7 @@ export interface RuntimeEvent {
   outcome?: FindingOutcome;
   ruleId?: string;
   references?: StandardReference[];
+  focusWalk?: FocusWalkResult;
 }
 
 export interface RuntimeInteraction {
